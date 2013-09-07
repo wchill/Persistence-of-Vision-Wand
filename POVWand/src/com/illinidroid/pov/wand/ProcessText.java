becfba019c5f6c5da82c9f657b8bd577f158bcd1
moriarty <moriarty@moriarty-PC>
2013-09-07 06:53:40 +0000

    updated process file

--- POVWand/src/com/illinidroid/pov/wand/ProcessText.java
+++ POVWand/src/com/illinidroid/pov/wand/ProcessText.java
@@ -7,7 +7,7 @@ import android.graphics.Paint;
 
 public class ProcessText {
 	
-	public boolean[][] processImage(String text, float textSize) {
+	public int[][] processImage(String text, float textSize) {
 		Paint paint = new Paint();
 
 	    int width = (int) (paint.measureText(text) + 0.5f); // round
@@ -18,28 +18,51 @@ public class ProcessText {
 	    canvas.drawText(text, 0, baseline, paint);
 	    Bitmap b = image;
 		int[] pixels = new int[b.getWidth()*b.getHeight()];
-		
-		b.getPixels(pixels, 0, b.getWidth(), 0, 0, b.getWidth()-1, b.getHeight()-1);
-		
-		System.out.println(Arrays.toString(pixels));
-		boolean[][] isColored = new boolean[b.getWidth()][b.getHeight()];
-		for(int a = 0; a < b.getHeight();a++)
+		/*boolean[][] */ int[][] isColored = new /*boolean*/ int[b.getHeight()][b.getWidth()];
+		System.out.println("1");
+		for(int r= 0; r < b.getHeight(); r++)
 		{
-			Arrays.fill(isColored[a],false);
-		}
-		int x = -1;
-		for(int r = 0; r < b.getHeight(); r++)
-		{
-			for(int c = 0; c < b.getWidth();c++ )
-			{   x+=1;
-				
-				if(pixels[x] != 0)
+			System.out.println("2");
+			for(int c = 0; c < b.getWidth(); c++)
+			{
+				System.out.println("3");
+				if(b.getPixel(c,r) != 0)
 				{
-				isColored[r][c] = true;
+					isColored[r][c] = 1;
 				}
+				
 			}
-		
 		}
+//	/*
+//		//b.getPixels(pixels, 0, b.getWidth(), 0, 0, b.getWidth(), b.getHeight());
+//		
+//		System.out.println(Arrays.toString(pixels));
+//		
+//		System.out.println(b.getWidth() + " " + b.getHeight());
+//		for(int a = 0; a < b.getHeight();a++)
+//		{
+//			Arrays.fill(isColored[a],0/*false*/);
+//			System.out.println(a);
+//			System.out.println("isColored Length:" + isColored[0].length);
+//			System.out.println("isColored Height:" + isColored.length);
+//		}
+//
+//		
+//		int x = -1;
+//		for(int r = 0; r < b.getHeight(); r++)
+//		{
+//			for(int c = 0; c < b.getWidth();c++ )
+//			{   x+=1;
+//				
+//				if(pixels[x] != 0)
+//				{
+//				isColored[r][c] = 1;//true;
+//				}
+//			}
+//		
+//		}
+//		
+//	
 		System.out.println(Arrays.deepToString(isColored));
 		return isColored;
 	}
