a691ffc39170e991c28796eda14757d818a51b6b
Eric Ahn <wchill1337@gmail.com>
2013-09-07 00:23:15 -0500

    ColorPicker and UI

---
+++ POVWand/src/com/illinidroid/pov/wand/DisplayActivity.java
@@ -0,0 +1,12 @@
+package com.illinidroid.pov.wand;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display);
+	}
+}
